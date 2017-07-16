7ac15e9820de0ecee65aa03a64d5a509744aa872
Lucas <rukaso.mero@gmail.com>
2017-07-16 13:50:59 -0300

    post

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,11 +1,14 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
@@ -14,11 +17,20 @@ import br.com.alura.loja.modelo.Carrinho;
 public class CarrinhoResource {
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
 	@Path("{id}")
 	public String busca(@PathParam(value="id")Long id) {
 	    Carrinho carrinho = new CarrinhoDAO().busca(id);
-	    return carrinho.toJSON();
+	    return carrinho.toXML();
 
+	}
+	
+
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo){
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        new CarrinhoDAO().adiciona(carrinho);
+        return "<status>sucesso</status>";
 	}
 }
--- loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,12 +1,16 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.thoughtworks.xstream.XStream;
+
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Projeto;
 
 @Path("projetos")
@@ -16,9 +20,10 @@ public class ProjetoResource {
 	
 	@Path("{id}")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") Long id){
 		Projeto projeto = projetoDAO.busca(id);
-		return projeto.toJSON();
+		return projeto.toXML();
 	}
+	
 }
